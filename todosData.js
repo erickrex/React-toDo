e6de49e9fce0b77aa7df85b3ade450c26b956ac6
erickrex <59777190+erickrex@users.noreply.github.com>
2020-11-19 23:58:18 +0100

    Todo App
    Still to be connected to a database or consume an API

---
+++ todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Take out the trash",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Grocery shopping",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Clean bathroom",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Clean fridge",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Code",
+        completed: false
+    }
+]
+
+export default todosData
